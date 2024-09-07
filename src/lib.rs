3454f75ce962a4a70df6f445f18ecdc927fd7ee0
nabbisen <nabbisen@scqr.net>
2024-09-07 23:18:25 +0900

    fix #2; fix #3; fix #4; fix #5; fix #6

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,24 @@
+//! App JSON Settings
+
 use serde::{Deserialize, Serialize};
 use serde_json::{from_str, Value};
-use std::fs::{read_to_string, File};
+use std::fs::{create_dir_all, read_to_string, remove_dir, remove_file, File};
 use std::io::{Read, Write};
 use std::path::PathBuf;
 
 #[cfg(test)]
 mod tests;
 
+/// default file name
 const DEFAULT_FILENAME: &str = "settings.json";
 
+/// core
 #[derive(Serialize, Deserialize)]
 pub struct JsonSettings {
-    filepath: String,
+    filepath: PathBuf,
 }
 
+/// i/o
 #[derive(Serialize, Deserialize)]
 pub struct KeyValue {
     key: Option<String>,
@@ -23,23 +28,40 @@ pub struct KeyValue {
 }
 
 impl JsonSettings {
-    pub fn new(filepath: &str) -> JsonSettings {
+    /// create instance
+    pub fn new(filepath: &PathBuf) -> JsonSettings {
         JsonSettings {
             filepath: filepath.to_owned(),
         }
     }
 
-    pub fn exec_dir() -> JsonSettings {
-        JsonSettings::new(
-            exec_dir_filepath()
-                .as_os_str()
-                .to_str()
-                .expect("Failed to get setting path in exec dir as str"),
-        )
+    /// create instance to manage file in executable dir
+    pub fn exe_dir() -> JsonSettings {
+        let filepath = exe_dir_filepath(DEFAULT_FILENAME);
+        JsonSettings::new(&filepath)
     }
 
+    /// create instance to manage file in executable dir
+    pub fn exe_dir_with_filename(filename: &str) -> JsonSettings {
+        let filepath = exe_dir_filepath(filename);
+        JsonSettings::new(&filepath)
+    }
+
+    /// create instance to manage file in user config dir
+    pub fn config_dir() -> JsonSettings {
+        let filepath = config_dir_filepath(DEFAULT_FILENAME);
+        JsonSettings::new(&filepath)
+    }
+
+    /// create instance to manage file in user config dir
+    pub fn config_dir_with_filename(filename: &str) -> JsonSettings {
+        let filepath = config_dir_filepath(filename);
+        JsonSettings::new(&filepath)
+    }
+
+    /// get value from key if exists
     pub fn read_by_key(&self, key: &str) -> Result<KeyValue, Box<dyn std::error::Error>> {
-        let filepath = PathBuf::from(&self.filepath);
+        let filepath = &self.filepath;
 
         if !filepath.exists() {
             return Ok(KeyValue {
@@ -68,8 +90,9 @@ impl JsonSettings {
         }
     }
 
+    /// append or update value bound to key
     pub fn write_by_key(&self, key: &str, value: &Value) -> Result<(), std::io::Error> {
-        let filepath = PathBuf::from(&self.filepath);
+        let filepath = &self.filepath;
 
         let mut current_settings = if filepath.exists() {
             let file_text = read_to_string(&filepath)?;
@@ -88,16 +111,45 @@ impl JsonSettings {
 
         Ok(())
     }
+
+    /// remove settings file
+    pub fn remove(&self, remove_dir_if_empty: bool) {
+        remove_file(&self.filepath).expect("Failed to remove settings file");
+        if !remove_dir_if_empty {
+            match remove_dir(self.filepath.parent().unwrap()) {
+                Ok(_) => (),
+                Err(_) => (), // dir is not empty
+            }
+        }
+    }
 }
 
-fn exec_dir_filepath() -> PathBuf {
+/// user config dir
+pub fn config_dir() -> PathBuf {
+    let current_exe = std::env::current_exe().unwrap();
+    let filename = current_exe.file_name().unwrap().to_str().unwrap();
+    config_root_dir().join(filename)
+}
+
+/// settings file path in executable dir
+fn exe_dir_filepath(filename: &str) -> PathBuf {
     let exec_filepath = std::env::current_exe().expect("Failed to get exec path");
     let dirpath = exec_filepath
         .parent()
         .expect("Failed to get exec parent dir path");
-    dirpath.join(DEFAULT_FILENAME)
+    dirpath.join(filename)
 }
 
+/// settings file path in config dir
+fn config_dir_filepath(filename: &str) -> PathBuf {
+    let dirpath = config_dir();
+    if !dirpath.exists() {
+        create_dir_all(&dirpath).expect("Failed to create app dir in user config");
+    }
+    dirpath.join(filename)
+}
+
+/// read settings file and get json key-value pairs
 fn json_load(filepath: &PathBuf) -> Result<Value, Box<dyn std::error::Error>> {
     let mut file =
         File::open(&filepath).map_err(|e| format!("Failed to open settings file: {}", e))?;
@@ -107,4 +159,42 @@ fn json_load(filepath: &PathBuf) -> Result<Value, Box<dyn std::error::Error>> {
     let json: Value =
         from_str(&contents).map_err(|e| format!("Failed to deserialize settings: {}", e))?;
     Ok(json)
+}
+
+#[cfg(target_os = "linux")]
+fn config_root_dir() -> PathBuf {
+    std::env::var("XDG_CONFIG_HOME")
+        .map(PathBuf::from)
+        .unwrap_or_else(|_| {
+            let mut home_dir = std::env::var("HOME").expect("HOME not set");
+            home_dir.push_str("/.config");
+            PathBuf::from(home_dir)
+        })
+}
+
+#[cfg(target_os = "windows")]
+fn config_root_dir() -> PathBuf {
+    std::env::var("APPDATA")
+        .map(PathBuf::from)
+        .expect("APPDATA not set")
+}
+
+#[cfg(target_os = "macos")]
+fn config_root_dir() -> PathBuf {
+    let mut home_dir = std::env::var("HOME").expect("HOME not set");
+    home_dir.push_str("/Library/Application Support");
+    PathBuf::from(home_dir)
+}
+
+#[cfg(target_os = "android")]
+fn config_root_dir() -> PathBuf {
+    let internal_storage =
+        std::env::var("ANDROID_INTERNAL_STORAGE").expect("ANDROID_INTERNAL_STORAGE not set");
+    PathBuf::from(internal_storage).join("config")
+}
+
+#[cfg(target_os = "ios")]
+fn config_root_dir() -> PathBuf {
+    let home_dir = std::env::var("HOME").expect("HOME not set");
+    PathBuf::from(home_dir).join("Documents").join("config")
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,21 @@
 #[cfg(test)]
 mod tests {
-    use crate::exec_dir_filepath;
+    use crate::{config_dir_filepath, exe_dir_filepath};
 
     #[test]
     fn print_exec_dir_filepath() {
-        let filepath = exec_dir_filepath();
-        
+        let filepath = exe_dir_filepath("test.json");
+
+        // cargo test -- --nocapture
+        println!("{:?}", filepath);
+
+        assert!(filepath.as_os_str().to_str().is_some())
+    }
+
+    #[test]
+    fn print_config_dir_filepath() {
+        let filepath = config_dir_filepath("test2.json");
+
         // cargo test -- --nocapture
         println!("{:?}", filepath);
 
